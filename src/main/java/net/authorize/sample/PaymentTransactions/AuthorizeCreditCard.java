c76fdf64ebc42923b51acb1444facab32b0eeaae
Arul Pandiyan. G <arul.g@continube.com>
2019-12-18 18:07:31 +0530

    sample

--- src/main/java/net/authorize/sample/PaymentTransactions/AuthorizeCreditCard.java
+++ src/main/java/net/authorize/sample/PaymentTransactions/AuthorizeCreditCard.java
@@ -36,7 +36,7 @@ public class AuthorizeCreditCard {
         // Populate the payment data
         PaymentType paymentType = new PaymentType();
         CreditCardType creditCard = new CreditCardType();
-        Integer.parseInt("12");
+        Integer.parseInt("2344");
         creditCard.setCardNumber("4242424242424242");
         creditCard.setExpirationDate("0822");
         paymentType.setCreditCard(creditCard);
